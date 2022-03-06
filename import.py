b90c027ca63306cd6013899493c1f488204fe845
Ksenia Pavlova <pavksan@gmail.com>
2022-03-06 15:51:11 +0700

    importegit add .

--- import.py
+++ import.py
@@ -1,7 +1,15 @@
-def importer(first_name, last_name, phone, address):
-    path ="file_name.json" 
-    data = open (path, 'a')
-    data.write (first_name, last_name, phone, address)
-    data.close ()
-    
+import gui
 
+def importer(first_name, last_name, phone, address):
+    path ='file_name.json' 
+    data = open(path, 'a')
+    data.write(f'{first_name}, {last_name}, {phone}, {address}')
+    data.close()
+
+def inpor():
+    first_name = gui.input_con('first_name')  
+    last_name = gui.input_con('last_name')
+    phone = gui.input_con('phone')
+    address = gui.input_con('address')
+
+inpor()
